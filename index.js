2c8a5f5c4a2555fcf6e09b0bb069da15906f6fe1
BexLedwith <bex.akeakamai@gmail.com>
2021-07-12 20:28:41 -0400

    finish js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-    <script>
-      let emailCollectorForm = document.getElementById("Email-Collector");
-      emailCollectorForm.addEventListener("submit", (event) => {
-        event.preventDefault();
+let emailCollectorForm = document.getElementById("Email-Collector");
+emailCollectorForm.addEventListener("submit", (event) => {
+  //   stop default event behavior
+  event.preventDefault();
+  // use form data to get user's name and email
+  let ourFormData = new FormData(event.target);
+  let userFirstName = ourFormData.get("name");
+  let userEmailAddress = ourFormData.get("email");
 
-        let ourFormData = new FormData(event.target);
-        let userFirstName = ourFormData.get("name");
-
-        let updatedHtmlContent = `
+  let updatedHtmlContent = `
             <h2>Congratulations, ${userFirstName}!</h2>
             <p>You're on your way to becoming a BBQ Master!
             </p>
            
             <p class="fine-print">
-                We'll never share your information without your permission
+                You will get weekly BBQ tips sent to: ${userEmailAddress}
             </p>
             `;
 
-        let ourMainContent = document.getElementById("main-content");
-        ourMainContent.innerHTML = updatedHtmlContent;
-      });
-    </script>
+  let updatedContentContainer = document.getElementById("main-content");
+  updatedContentContainer.innerHTML = updatedHtmlContent;
+});
