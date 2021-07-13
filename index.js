1eb026cc0f17fd3f6c5140c9875b0b73b048fa28
BexLedwith <bex.akeakamai@gmail.com>
2021-07-12 20:23:53 -0400

    add js

---
+++ index.js
@@ -0,0 +1,22 @@
+    <script>
+      let emailCollectorForm = document.getElementById("Email-Collector");
+      emailCollectorForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        let ourFormData = new FormData(event.target);
+        let userFirstName = ourFormData.get("name");
+
+        let updatedHtmlContent = `
+            <h2>Congratulations, ${userFirstName}!</h2>
+            <p>You're on your way to becoming a BBQ Master!
+            </p>
+           
+            <p class="fine-print">
+                We'll never share your information without your permission
+            </p>
+            `;
+
+        let ourMainContent = document.getElementById("main-content");
+        ourMainContent.innerHTML = updatedHtmlContent;
+      });
+    </script>
